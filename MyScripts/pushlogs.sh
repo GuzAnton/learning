9bbc75df0c40df205e4a0c97506970597e628e93
GuzAnton <106443030+GuzAnton@users.noreply.github.com>
2022-09-14 22:13:18 +0300

    Update pushlogs.sh

--- MyScripts/pushlogs.sh
+++ MyScripts/pushlogs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 while read text; do
 echo $text
-done < /var/log/nginx/access.log >> /home/tony/GitProject/learning/MyScripts/logfile.csv
+done < /var/log/nginx/access.log"."* >> /home/tony/GitProject/learning/MyScripts/logfile.csv
 
 while read text; do
 echo $text
-done < /var/log/nginx/error.log >> /home/tony/GitProject/learning/MyScripts/logfile.csv
+done < /var/log/nginx/error.log"."* >> /home/tony/GitProject/learning/MyScripts/logfile.csv
 
 git add logfile.csv
 git commit -m "new logs from nginx"
