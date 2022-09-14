be69e3901d8405dbb03b10d5bdbd060a23d9631e
GuzAnton <Guzantony@gmail.com>
2022-09-14 21:39:48 +0300

    modified script

--- MyScripts/pushlogs.sh
+++ MyScripts/pushlogs.sh
@@ -1,13 +1,14 @@
-#!bin/bash
-
+#!/bin/bash
 while read text; do
 echo $text
 done < /var/log/nginx/access.log >> /home/tony/GitProject/learning/MyScripts/logfile.csv
+
 
 while read text; do
 echo $text
 done < /var/log/nginx/error.log >> /home/tony/GitProject/learning/MyScripts/logfile.csv
 
 git add logfile.csv
-git commit -m "mv server.log.logs/$(date -d "today" + "%Y%m%d%H%M").log"
-git push
+git commit -m "new logs from nginx"
+git push origin
+
