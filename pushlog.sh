ca80349168d03c1467d24d377aed24001ce2dc7d
GuzAnton <Guzantony@gmail.com>
2022-09-15 09:59:57 +0300

    final version pushlog.sh

--- pushlog.sh
+++ pushlog.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 while read text; do
 echo $text
-done < /var/log/nginx/access.log?? >> /home/tony/GitProject/learning/MyScripts/logfile.csv
+done < /var/log/nginx/access.log* >> /home/tony/GitProject/learning/MyScripts/logfile.csv
 
 while read text; do
 echo $text
-done < /var/log/nginx/error.log?? >> /home/tony/GitProject/learning/MyScripts/logfile.csv
+done < /var/log/nginx/error.log* >> /home/tony/GitProject/learning/MyScripts/logfile.csv
 
-#git add logfile.csv
-#git commit -m "new logs from nginx"
-#git push origin
+git add logfile.csv
+git commit -m "new logs from nginx"
+git push origin master
